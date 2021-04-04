5de128bf2351719eb3ef81462e6ab5a4be311f33
Matias Rivera <juanmatiasrivera@outlook.com>
2021-04-04 02:55:11 -0300

    Auth
    Added Login
    Styled Register and Login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import './App.scss';
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Chat from './components/Chat/Chat'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <div>App</div>
+      <Router>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/' exact component={Chat} />
+          <Route render={() => <h1>Page not found</h1>} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Auth/Login.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import loginImage from '../../assets/images/login.svg'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+import AuthServices from '../..//services/authServices'
+
+import './Auth.scss'
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitForm = (e) => {
+        e.preventDefault()
+
+        AuthServices.login({email, password}).then(res => console.log(res))
+    }
+
+    return ( 
+        <div className='auth-background'>
+            <div className='auth-container'>
+                <div className='auth-card card-shadow'>
+                    <div className='auth-card__image'>
+                        <img src={loginImage} alt='Login'/>
+                    </div>
+
+                    <div className='auth-card__form'>
+                        <h2>Welcome back</h2>
+
+                        <form onSubmit={submitForm}>
+                            <div className='input-field mb-1'>
+                                <input 
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    required='required'
+                                    type='text'
+                                    placeholder='Email'
+                                />
+                            </div>
+
+                            <div className='input-field mb-2'>
+                                <input
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    required='required'
+                                    type='password' 
+                                    placeholder='Password' 
+                                />
+                            </div>
+
+                            <button>LOGIN</button>
+                        
+                            <p>Don't have and account? <Link to='/register'>Register</Link></p>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
